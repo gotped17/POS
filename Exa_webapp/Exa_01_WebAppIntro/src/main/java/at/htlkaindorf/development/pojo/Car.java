31d0b5df35b9df72e9f7f27d974a46d424dba39d
gotped17 <gotped17@users.noreply.github.com>
2020-11-18 11:51:14 +0100

    Intro to json

--- Exa_webapp/Exa_01_WebAppIntro/src/main/java/at/htlkaindorf/development/pojo/Car.java
+++ Exa_webapp/Exa_01_WebAppIntro/src/main/java/at/htlkaindorf/development/pojo/Car.java
@@ -5,6 +5,11 @@
  */
 package at.htlkaindorf.development.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,13 +26,22 @@ import lombok.ToString;
 public class Car {    
     private String brand;
     private String type;
-    @EqualsAndHashCode.Exclude //Excluded die Variabler aus der equals und hashcode methode
-    @ToString.Exclude //Excluded die Variable aus der toString methode
+    // @EqualsAndHashCode.Exclude //Excluded die Variabler aus der equals und hashcode methode
+    // @ToString.Exclude //Excluded die Variable aus der toString methode
+    // @JsonIgnore // jackson ignoriert diese Variable beim writen
     private double weight;
     
     public static void main(String[] args) {
         Car car = new Car();
         Car car2 = new Car("vw", "polo", 1234);
         System.out.println(car2.toString());
+        Car[] cars = {car, car2};
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonStr = mapper.writeValueAsString(cars);
+            System.out.println(jsonStr);
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(Car.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
